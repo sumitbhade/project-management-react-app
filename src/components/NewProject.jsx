ebb2675c6f200a8b50cedfe0e287211773ae7446
Sumit Bhade <imsumitbhade@gmail.com>
2024-12-21 20:32:28 +0530

    Handling NoProjectSelected and splitting Button

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,20 +1,20 @@
 import Input from "./Input";
+import Button from "./Button";
 
 export default function NewProject() {
   return (
     <div className="w-[35rem] mt-16">
       <menu className="flex items-center justify-end gap-4 my-4">
         <li>
-          <button className="text-stone-800 hover:text-stone-950">
+          <Button className="text-stone-800 hover:text-stone-950">
             Cancel
-          </button>
+          </Button>
         </li>
         <li>
-          <button className="px-6 py-2 rounded-md text-stone-50 bg-stone-800 hover:bg-stone-950">
-            Save
-          </button>
+          <Button>Save</Button>
         </li>
       </menu>
+
       <div>
         <Input label="Title" />
         <Input label="Description" isTextArea />
