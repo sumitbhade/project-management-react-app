c83c5fab3aa3e239ae5408512e4486d7f0951238
Sumit Bhade <imsumitbhade@gmail.com>
2024-12-22 00:36:54 +0530

    Showing Error Modal On Invalid Input

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,16 +1,27 @@
 import Input from "./Input";
 import Button from "./Button";
 import { useRef } from "react";
+import Modal from "./Modal";
 
 export default function NewProject({ onAdd }) {
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
+  const modal = useRef();
 
   function handleSave() {
     const enteredTitle = title.current.value;
     const enteredDescription = description.current.value;
     const enteredDueDate = dueDate.current.value;
+
+    if (
+      enteredTitle.trim() === "" ||
+      enteredDescription.trim() === "" ||
+      enteredDueDate.trim() === ""
+    ) {
+      modal.current.open();
+      return;
+    }
 
     onAdd({
       title: enteredTitle,
@@ -20,23 +31,29 @@ export default function NewProject({ onAdd }) {
   }
 
   return (
-    <div className="w-[35rem] mt-16">
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li>
-          <Button className="text-stone-800 hover:text-stone-950">
-            Cancel
-          </Button>
-        </li>
-        <li>
-          <Button onClick={handleSave}>Save</Button>
-        </li>
-      </menu>
+    <>
+      <Modal ref={modal} buttonCaption="Close">
+        <h2>Invalid input</h2>
+        <p>Oops! You forgot to enter all values</p>
+      </Modal>
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <Button className="text-stone-800 hover:text-stone-950">
+              Cancel
+            </Button>
+          </li>
+          <li>
+            <Button onClick={handleSave}>Save</Button>
+          </li>
+        </menu>
 
-      <div>
-        <Input ref={title} type="text" label="Title" />
-        <Input ref={description} label="Description" isTextArea />
-        <Input ref={dueDate} type="date" label="Due Date" />
+        <div>
+          <Input ref={title} type="text" label="Title" />
+          <Input ref={description} label="Description" isTextArea />
+          <Input ref={dueDate} type="date" label="Due Date" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
