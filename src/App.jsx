ccd90888d0e2554bada997fe40d7b111ae155743
Sumit Bhade <imsumitbhade@gmail.com>
2024-12-22 00:14:48 +0530

    Handling Project Creation

--- src/App.jsx
+++ src/App.jsx
@@ -20,15 +20,16 @@ function App() {
 
   function handleAddProject(projectData) {
     setProjectState((prevState) => {
+      const projectId = Math.random();
       const newProject = {
         ...projectData,
-        id: Math.random(),
+        id: projectId,
       };
 
       return {
         ...prevState,
-        projects: [...prevState.projects],
-        newProject,
+        projectStateId: "not adding",
+        projects: [...prevState.projects, newProject],
       };
     });
   }
@@ -43,7 +44,10 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectState.projects}
+      />
       {content}
     </main>
   );
